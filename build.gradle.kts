0f152b2b8b7df0d29dcb150b2c5f885e58400bfd
abu naser <75668297+naser09@users.noreply.github.com>
2025-07-28 19:48:00 +0600

    init

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "naser09.github.io.dsa"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
